ab395da6c7716b27ebc418ab10be6ab85598115b
Brian342 <migelbrian3@gmail.com>
2024-03-16 04:54:32 +0300

    abstract classes

---
+++ Abstract.java
@@ -0,0 +1,12 @@
+public class Abstract {
+    public static void main(String[] args) {
+        /* abstract = abstaract classes cannot be instantiated, but they can have a subclass
+         *            abstaract methods are declared withiout an implementation
+         */
+        carrs car = new carrs();
+        //vehicles vehicle = new vehicles();
+
+        car.go();
+
+    }
+}
