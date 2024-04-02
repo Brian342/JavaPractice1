38b06187cca866bc303ac910ce2504c6e07cb272
Brian342 <migelbrian3@gmail.com>
2024-04-02 12:16:23 +0300

    interfaces

---
+++ Hawk.java
@@ -0,0 +1,8 @@
+public class Hawk implements predator{
+
+    @Override
+    public void hunt() {
+        System.out.println("The hawk is hunting!");
+    }
+    
+}
---
+++ Rabbit.java
@@ -0,0 +1,8 @@
+public class Rabbit implements prey {
+
+    @Override
+    public void flee() {
+        System.out.println("*The rabbit is fleeing*");
+    }
+    
+}
