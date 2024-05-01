b16cae18daeb79ec3ea7807d143879430f832bef
Brian342 <migelbrian3@gmail.com>
2024-05-01 18:19:47 +0300

    Jframe

---
+++ Jframe.java
@@ -0,0 +1,27 @@
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Jframe {
+    public static void main(String[] args) {
+
+        /* Jframe = a GUI window to add components to */
+
+        JFrame frame = new JFrame(); //creates a frame
+        frame.setTitle("JFrame title goes here");//sets title for frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// exit out of application
+        frame.setResizable(false);//prevent frame from being resized
+        frame.setSize(420,420); //sets the x and y dimension of frame
+        frame.setVisible(true);// make frame visible
+
+        ImageIcon image = new ImageIcon("logo.png");// create an ImageIcon
+        frame.setIconImage(image.getImage());//change Icon of Frame
+        // frame.getContentPane().setBackground(Color.gray);//change color of background
+        // frame.getContentPane().setBackground(new Color(0,255,255));
+        frame.getContentPane().setBackground(new Color(0x123456));//using hexadecimal
+
+
+  
+}
+}
