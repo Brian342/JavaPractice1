0996afa113e3617044956a6684d7f51da111d775
Brian342 <migelbrian3@gmail.com>
2024-05-15 18:02:17 +0300

    java

---
+++ Textfield.java
@@ -0,0 +1,8 @@
+public class Textfield {
+    public static void main(String[] args) {
+          /*JTextfield = a gui textbox component that can be used to add, set, or get text */
+            textfieldframe frame = new textfieldframe();
+    }
+  
+
+}
