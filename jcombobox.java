10f4861d289cd4b348a3f398030397f179a62e03
Brian342 <migelbrian3@gmail.com>
2024-05-18 05:43:11 +0300

    java

---
+++ jcombobox.java
@@ -0,0 +1,10 @@
+public class jcombobox {
+    public static void main(String[] args) {
+        
+        /*Jcombobox = A component that combines a button or 
+        editable field and a drop-down list */
+
+       new jcomboboxframe();
+
+    }
+}
---
+++ jcomboboxframe.java
@@ -0,0 +1,13 @@
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+
+public class jcomboboxframe extends JFrame implements ActionListener{
+    
+    jcomboboxframe(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.setVisible(true);
+    }
+    }
+   
