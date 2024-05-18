8355c8021eedf24741f92794c70bab5e90e523b5
Brian342 <migelbrian3@gmail.com>
2024-05-18 05:57:23 +0300

    java

--- jcombobox.java
+++ jcombobox.java
@@ -1,7 +1,7 @@
 public class jcombobox {
     public static void main(String[] args) {
         
-        /*Jcombobox = A component that combines a button or 
+        /* Jcombobox = A component that combines a button or 
         editable field and a drop-down list */
 
        new jcomboboxframe();
--- jcomboboxframe.java
+++ jcomboboxframe.java
@@ -1,13 +1,45 @@
 import java.awt.FlowLayout;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class jcomboboxframe extends JFrame implements ActionListener{
     
+    JComboBox comboBox;
+    @SuppressWarnings("unchecked")
     jcomboboxframe(){
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
+
+        String[] animals = {"dog", "cat", "bird"};
+
+       comboBox = new JComboBox(animals);
+       comboBox.addActionListener(this);
+
+       comboBox.setEditable(true);
+      System.out.println( comboBox.getItemCount());
+      comboBox.addItem("Horse");
+      comboBox.insertItemAt("Pig", 0);
+    //   comboBox.removeItem("bird");
+    //   comboBox.setSelectedIndex(0);
+    //   comboBox.removeItemAt(0);
+    //   comboBox.removeAllItems();
+      
+
+        this.add(comboBox);
+        this.pack();
         this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==comboBox){
+        //    System.out.println(comboBox.getSelectedItem()); 
+        System.out.println(comboBox.getSelectedIndex());
+        }
     }
     }
    
