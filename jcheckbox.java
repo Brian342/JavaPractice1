12d5a5ea1185d82edd744f1319221cdb3bf22321
Brian342 <migelbrian3@gmail.com>
2024-05-18 05:04:57 +0300

    java

---
+++ jcheckbox.java
@@ -0,0 +1,7 @@
+public class jcheckbox {
+    public static void main(String[] args) {
+        /*JcheckBox = A Gui component that can be selected or deselected */
+
+        new jcheckboxframe();
+    }
+}
---
+++ jcheckboxframe.java
@@ -0,0 +1,48 @@
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+
+public class jcheckboxframe  extends JFrame implements ActionListener{
+    
+    JButton button;
+    JCheckBox checkBox;
+    ImageIcon xicon;
+    ImageIcon checkIcon;
+
+
+    jcheckboxframe(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        xicon = new ImageIcon("wrong");
+        checkIcon = new ImageIcon("tick");
+
+        button = new JButton();
+        button.setText("Submit");
+        button.addActionListener(this);
+
+        checkBox = new JCheckBox();
+        checkBox.setText("I agree to the terms and conditions");
+        checkBox.setFocusable(false);
+        checkBox.setFont(new Font("Consolas",Font.PLAIN,20));
+        checkBox.setIcon(xicon);
+        checkBox.setSelectedIcon(checkIcon);
+
+        this.add(button);
+        this.add(checkBox);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == button){
+           System.out.println(checkBox.isSelected()); 
+    }
+}
+}
