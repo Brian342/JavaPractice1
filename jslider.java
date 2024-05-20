2c7ff032c8bb604486ec7ed4af4a5c18f9809e1a
Brian342 <migelbrian3@gmail.com>
2024-05-21 01:33:57 +0300

    java

--- jslider.java
+++ jslider.java
@@ -1,5 +1,9 @@
 public class jslider {
     public static void main(String[] args) {
-        
+        /*JSlider = GUI component that lets user enter a value 
+         *          by using an adjustable sliding knob an a track  
+         */
+
+         jsliderdemo sliderdem0 = new jsliderdemo();
     }
 }
