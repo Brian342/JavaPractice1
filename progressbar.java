803bc6826d1de962662035b1e3e15cfeba35b15e
Brian342 <migelbrian3@gmail.com>
2024-05-21 02:01:46 +0300

    java

--- progressbar.java
+++ progressbar.java
@@ -1,3 +1,7 @@
 public class progressbar {
-    
+    public static void main(String[] args) {
+        /* progress bar = visual aid to let the user know that an operation is processing */
+
+        progressbardemo progressbar = new progressbardemo();
+    }
 }
