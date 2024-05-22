2d5f12a28fe729a90393eca78fedfcb92401d0a8
Brian342 <migelbrian3@gmail.com>
2024-05-22 04:42:30 +0300

    mouselisterner

--- mouselisternerframe.java
+++ mouselisternerframe.java
@@ -1,40 +1,62 @@
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class mouselisternerframe extends JFrame implements MouseListener{
     
+    JLabel label;
+
     mouselisternerframe(){
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
         this.setLayout(null);
+
+        label = new JLabel();
+        label.setBounds(0,0,100,100);
+        label.setBackground(Color.red);
+        label.setOpaque(true);
+        label.addMouseListener(this);
+
+        this.add(label);
         this.setVisible(true);
+
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         // Invoked when the mouse button has been clicked (pressed and released) on a component
+        System.out.println("Mouse Clicked");
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
         //Invoked when a mouse button has been pressed on a component
+        System.out.println("Mouse Pressed");
+        label.setBackground(Color.yellow);
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
         //Invoked when a mouse button has been released on a computer
+        System.out.println("You released the mouse");
+        label.setBackground(Color.red);
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
         //Invoked when the mouse enters a component
+        System.out.println("You Entered Component");
+        label.setBackground(Color.green);
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
         //Invoked when the mouse exits a component
+        System.out.println("You Exited");
+        label.setBackground(Color.blue);
     }
 }
