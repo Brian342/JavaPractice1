c6ccfa399742e7fdc8e7e61c4ef45e86249549c3
Brian342 <migelbrian3@gmail.com>
2024-05-22 03:25:31 +0300

    Jfile

--- jfilechooser.java
+++ jfilechooser.java
@@ -1,4 +1,8 @@
 public class jfilechooser {
     /* JFilechooser = A Gui mechanism that let's user choose a file (helpful for opening or saving files) */
-    
+
+    public static void main(String[] args) {
+        
+        new jfilechooserframe();
+    }
 }
