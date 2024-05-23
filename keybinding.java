143a303bc3c8108dc0bd2a992812bac22431b8fa
Brian342 <migelbrian3@gmail.com>
2024-05-23 05:37:55 +0300

    keybinding

---
+++ keybinding.java
@@ -0,0 +1,9 @@
+public class keybinding {
+    /*Key Binding = bind an action to a keystroke
+     *              don't require you to click a component to give it focus
+     *              all swing components use key bindings
+     *              increased flexibility compared to keylisteners
+     *              can assign key stroke to individual swing components
+     *              more difficult to utilize and set up :(
+     */
+}
