33ce680020cb719902f17ff281fca70ea1a10bbc
Brian342 <migelbrian3@gmail.com>
2024-05-23 06:28:38 +0300

    2dgraphics

---
+++ twodghaphicsframe.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class twodghaphicsframe extends JFrame{
+   
+    twodghaphicsframe(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+
+    }
+}
---
+++ twodgraphics.java
@@ -0,0 +1,5 @@
+public class twodgraphics {
+    public static void main(String[] args) {
+        
+    }
+}
