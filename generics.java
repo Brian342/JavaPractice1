45aa5514d59a27fdff3b1f1b5bc4c29f10e6a823
Brian342 <migelbrian3@gmail.com>
2024-05-28 11:25:53 +0300

    generics

---
+++ generics.java
@@ -0,0 +1,10 @@
+public class generics {
+    public static void main(String args[]){
+        /*generics = enables types (classes and interfaces) to be parameters when defining:
+         *           classes, interfaces and methods
+         *           a benefit is to eliminate the need to create multiple versions 
+         *           of methods or classes for various data types.
+         *           use 1 version for all reference data types.
+        */
+    }
+}
