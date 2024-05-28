a10d44213400a8921e98fdb064aa58a4d999fd2c
Brian342 <migelbrian3@gmail.com>
2024-05-28 12:43:58 +0300

    serialization

---
+++ serialization.java
@@ -0,0 +1,15 @@
+public class serialization{
+    public static void main(String[] args){
+        /*Serialization = . The process of converting an object into a byte Stream
+         *                  persists (saves the state) the object after program exits
+         *                  this byte stream can be saved as a file or sent over a network
+         *                  can be sent to a different machine
+         *                  Byte Stream can be saved as a file (.ser) which is platform independenet
+         *                  (Think of this as if you're saving a file with the object's information)
+         * 
+         * 
+         * Deserialization = the reverse process of converting a byte stream into an object.
+         *                   (Think of this as if you're loading a saved file)
+         */
+    }
+}
