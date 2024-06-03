8f2e48edb2f3d7709bba9f90b064bc7b58ae02d2
Brian342 <migelbrian3@gmail.com>
2024-06-04 00:29:36 +0300

    java threads

---
+++ Thread.java
@@ -0,0 +1,15 @@
+public class Thread {
+    public static void main(String[] args) throws InterruptedException{
+        /*
+         * Thread = A thread of execution in a program (kind of like a virtual CPU)
+         *          The JVM allows an application to have multiple threads running concurrently
+         *          Each thread can execute parts of your code in parallel with the main thread
+         *          Each thread has a priority.
+         *          Threads with higher priority are executed in preference compared to threads with a lower priority
+         * 
+         *          The java virtual machine continues to execute threads unitil either of the following occurs
+         *              1. The exit method of a class Runtime has been called
+         *              2. All user threads have died
+         */
+    }
+}
