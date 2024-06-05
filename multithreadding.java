fa46192bf810777598219c42656397eeae5cd6ce
Brian342 <migelbrian3@gmail.com>
2024-06-05 03:49:55 +0300

    threading

--- multithreadding.java
+++ multithreadding.java
@@ -1,5 +1,5 @@
 public class multithreadding {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /* multithreading = process of executing multiple threads simultaneously
                             helps maximum utilization of cpu
                             Threads are independent, they don't affect the execution of other threads
@@ -12,8 +12,13 @@ public class multithreadding {
         mymultithreadingrunnable runnable1 = new mymultithreadingrunnable();
         Thread thread2 = new Thread(runnable1);
 
+        
+
         thread1.start();
+        thread1.join(3000); //calling thread (ex.main) wait untill the specified thread dies of for x milliseconds
         thread2.start();
+
+        // System.out.println(1/0);
 
     }
 }
