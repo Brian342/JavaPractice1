5d3d2e3dd3bcef2d63600cb6778d71cb0927eb62
Brian342 <migelbrian3@gmail.com>
2024-06-05 04:05:14 +0300

    toolbox

---
+++ practice.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import Tools.Toolbox;
+import javax.swing.Icon;
+public class practice {
+    public static void main(String[] args) {
+        
+        Toolbox = toolbox = new Toolbox();
+    }
+    
+}
