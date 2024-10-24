c97ab00fd0873c3522d91ee7a5728cb7f7da5202
Brian kimanzi <migelbrian3@gmail.com>
2024-10-24 16:54:19 +0300

    commit new changes

--- sqlConnector.java
+++ sqlConnector.java
@@ -1,30 +1,31 @@
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-public class sqlConnector {
+// import java.util.logging.Level;
+// import java.util.logging.Logger;
+// import java.sql.Connection;
+// import java.sql.DriverManager;
+// import java.sql.SQLException;
+// import java.sql.Statement;
+// public class sqlConnector {
     
-    public static void main(String[] args) {
-        sqlConnector pro = new sqlConnector();
-        pro.createconnector();
-    }
-   void createconnector(){
-    try {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("pass", "root", "root");
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("pass");
-        while (rs.next()) {
-            String name = rs.getString("");
-            System.out.println();
-        }
-        System.out.println("Connected");
-    } catch (ClassNotFoundException ex) {
-        Logger.getLogger(sqlConnector.class.getName()).log(Level.SEVERE, null, ex);
-        // TODO: handle exception
-    } catch (SQLException ex){
-        Logger.getLogger(sqlConnector.class.getName()).log(Level.SEVERE, null, ex);
-    }
-   }
-}
+//     public static void main(String[] args) {
+//         sqlConnector pro = new sqlConnector();
+//         pro.createconnector();
+//     }
+//    void createconnector(){
+//     try {
+//         Class.forName("com.mysql.cj.jdbc.Driver");
+//         Connection con = DriverManager.getConnection("", "SA", "<YourStrong@Passw0rd>");
+//         // Statement stmt = con.createStatement();
+//         // ResultSet rs = stmt.executeQuery("pass");
+//         // while (rs.next()) {
+//         //     String name = rs.getString("");
+//         //     System.out.println();
+//         // }
+//         System.out.println("Connected");
+//     } catch (ClassNotFoundException ex) {
+//         Logger.getLogger(sqlConnector.class.getName()).log(Level.SEVERE, null, ex);
+//         // TODO: handle exception
+//     } catch (SQLException ex){
+//         Logger.getLogger(sqlConnector.class.getName()).log(Level.SEVERE, null, ex);
+//     }
+//    }
+// }
---
+++ static/main.java
@@ -0,0 +1,12 @@
+public class main {
+
+    public static void main(String[] args){
+
+        Friend friend1 = new Friend("Spongebob");
+        Friend friend2 = new Friend("Patrick");
+        Friend friend3 = new Friend("Squidward");
+
+        System.out.println(Friend.numberOfFriends);
+    }
+
+}
